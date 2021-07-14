c8a95ffc826753255951fefa94728693050be4e2
Houston <houstonkoester@aol.com>
2021-07-14 12:47:57 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { MyAgmExampleComponent } from './my-agm-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    MyAgmExampleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBPFD6mRJC50nZ7dHzziZ2nGLe6aaee7K0'
+    })
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
---
+++ src/app/my-agm-example.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-agm-example',
+  template: `
+    <agm-map
+      [latitude]="lat"
+      [longitude]="lng"
+      [mapTypeId]="mapType"
+    >
+    </agm-map>
+  `,
+  styles: [
+    'agm-map { height: 300px; }'
+  ]
+})
+export class MyAgmExampleComponent implements OnInit {
+  lat = 21.3069;
+  lng = -157.8583;
+  mapType = 'satellite';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
