450750697a5a62dd6162d56b22ba61d2dab8bc49
Houston <houstonkoester@aol.com>
2021-07-17 19:08:27 -0700

    example details found mainly in app-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
-import { MyAgmExampleComponent } from './my-agm-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent,
-    MyAgmExampleComponent
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCheckboxModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBPFD6mRJC50nZ7dHzziZ2nGLe6aaee7K0'
-    })
+    MatCheckboxModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
